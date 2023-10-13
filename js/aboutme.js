4f6f8c08600c0e15e89bd29135e1c5f81f25df67
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2023-10-13 14:59:16 +0100

    d

--- js/aboutme.js
+++ js/aboutme.js
@@ -53,12 +53,6 @@ class AboutPage extends DefaultPage
         super("Salvatore Amaddio Rivolta", 651.5);
         this.addForm(new Form());
     }
-
-    submitForm() {  
-//        this.#form.submit();
-//        this.#form.reset(); 
-        return false;
-     }
 }
 
 new AboutPage();
